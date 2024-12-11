75296f1551eb9cd1fc2b630dbe62d46c9677e36a
Maia Weingard <maiawein@usc.edu>
2024-12-11 15:20:49 -0800

    Update UserLogOutServlet.java

--- src/main/java/com/collegeapp/UserLogOutServlet.java
+++ src/main/java/com/collegeapp/UserLogOutServlet.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-//@WebServlet("/UserLogOutServlet")
+@WebServlet("/UserLogOutServlet")
 public class UserLogOutServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
@@ -17,20 +17,31 @@ public class UserLogOutServlet extends HttpServlet
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		response.getWriter().write("UserLogOutServlet is running.");
+		response.getWriter().append("UserLogOutServlet is running.");
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		HttpSession session = request.getSession(false);
-
-		// Invalidate the session to log out the user.
-		if (session != null)
+		response.setContentType("application/json");
+		try
 		{
-			session.invalidate();
-		}
+			HttpSession session = request.getSession(false);
 
-		response.setStatus(HttpServletResponse.SC_OK);
+			// Invalidate the session to log out the user.
+			if (session != null)
+			{
+				session.invalidate();
+			}
+
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.getWriter().write(new Gson().toJson("Logged out successfully."));
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write(new Gson().toJson("An error occurred."));
+		}
 	}
 }
